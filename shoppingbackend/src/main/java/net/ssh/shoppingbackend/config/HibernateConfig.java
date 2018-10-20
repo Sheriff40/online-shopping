9cb45063b5d95644f2eee54e503d2d1aea80261f
Sheriff <Yamakuzi@DESKTOP-SOKUM2R>
2018-10-20 19:54:53 +0545

    heroki III

--- shoppingbackend/src/main/java/net/ssh/shoppingbackend/config/HibernateConfig.java
+++ shoppingbackend/src/main/java/net/ssh/shoppingbackend/config/HibernateConfig.java
@@ -19,9 +19,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class HibernateConfig {
 		
 	private static final String driverClassName = "org.postgresql.Driver";
-	private static final String URL = "jdbc:postgresql://ec2-23-21-171-249.compute-1.amazonaws.com/d1nc449sckrgjn?sslmode=require";
-	private static final String username = "ixlfrhwteqtgfm";
-	private static final String password = "fa6e379534f568be03316f47d975ae1e6f61920d0988f8fc93f40e871ffda07a";
+	private static final String URL = "jdbc:postgresql://ec2-54-163-235-56.compute-1.amazonaws.com/d22slmmimmgpc?sslmode=require";
+	private static final String username = "cigemprjlolcgk";
+	private static final String password = "0a81ecd578a6f524f81d409fa443f6c44b0b9112f73cb8fcbd632f6b6fd5a9e8";
 	
 	@Bean(name = "dataSource")
 	public DataSource getDataSource()
