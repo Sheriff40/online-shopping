cf6ef56b4b7a1b51429a143ff4730460e6589c9e
Sheriff <Yamakuzi@DESKTOP-SOKUM2R>
2018-10-22 13:36:30 +0545

    Addes postgreSQL instead in replace of H2

--- shoppingbackend/src/main/java/net/ssh/shoppingbackend/config/HibernateConfig.java
+++ shoppingbackend/src/main/java/net/ssh/shoppingbackend/config/HibernateConfig.java
@@ -18,10 +18,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class HibernateConfig {
 		
-	private static final String driverClassName = "org.h2.Driver";
-	private static final String URL = "jdbc:h2:tcp://localhost/~/online";
-	private static final String username = "sa";
-	private static final String password = "";
+	private static final String driverClassName = "org.postgresql.Driver";
+	private static final String URL = "jdbc:postgresql://ec2-54-163-235-56.compute-1.amazonaws.com/d22slmmimmgpc?sslmode=require";
+	private static final String username = "cigemprjlolcgk";
+	private static final String password = "0a81ecd578a6f524f81d409fa443f6c44b0b9112f73cb8fcbd632f6b6fd5a9e8";
 	
 	@Bean(name = "dataSource")
 	public DataSource getDataSource()
@@ -49,7 +49,7 @@ public class HibernateConfig {
 	private Properties getHibernateProperties() {
 		
 		Properties properties = new Properties();
-		properties.put("hibernate.dialect","org.hibernate.dialect.H2Dialect");
+		properties.put("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");
 		properties.put("hibernate.show_sql", true);
 		properties.put("hibernate.format_sql", true);
 		properties.put("hibernate.hbm2ddl.auto","update");
